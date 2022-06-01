07f056748255a43410c79466d497bb5907eeffe3
WuHaiheng <wuhh@csco.com.cn>
2022-06-01 15:28:42 +0800

    图片控件开发

--- lib/resource_page.dart
+++ lib/resource_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_color_plugin/flutter_color_plugin.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ResourcePage extends StatefulWidget {
   const ResourcePage({Key? key}) : super(key: key);
@@ -25,12 +29,37 @@ class _ResourcePageState extends State<ResourcePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
-            Image(
-              image: AssetImage('images/girl.png'),
-            ),
+            // const Image(
+            //   image: AssetImage('images/girl.png'),
+            // ),
+            // FutureBuilder(
+            //   future: _getLocalFile("Download/Stack.png"),
+            //   builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+            //     return snapshot.hasData
+            //         ? Image.file(snapshot.data!)
+            //         : Container();
+            //   },
+            // ),
+            // FadeInImage.memoryNetwork(
+            //     placeholder: kTransparentImage,
+            //     image:
+            //         'http://ww1.sinaimg.cn/large/0065oQSqly1frepqtwifwj30no0ti47n.jpg'),
+            // CachedNetworkImage(
+            //   placeholder:  (context, url) => const CircularProgressIndicator(),
+            //   imageUrl:'http://ww1.sinaimg.cn/large/0065oQSqly1frepqtwifwj30no0ti47n.jpg',
+            // ),
+            // Icon(Icons.home,size: 300),
+            Icon(IconData(0xf5566,fontFamily: "RubikMonoOne"),size: 200,
+              color: Colors.blue,)
           ],
         ),
       ),
     );
+  }
+
+  Future<File> _getLocalFile(String fileName) async {
+    String? dir = (await getExternalStorageDirectory())?.path;
+    File f = File('$dir/$fileName');
+    return f;
   }
 }
